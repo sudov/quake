e0cd31ade73b0de78e5b1b0d0308cb1039770735
sudov <vinay.farias@gmail.com>
2014-05-27 22:04:39 -0400

    added txt to json

---
+++ dbgen.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import Gson;
+
+public static void main(String[] args){
+	
+	BufferedReader reader = new BufferedReader(new FileReader("cities1000.txt"));
+	HashMap<String, String> myList;
+	BufferedWriter writer = new BufferedWriter(new FileWriter(outputfilepath));
+
+	while(reader.read()){
+		System
+	}
+	Gson gson = new Gson();
+	gson.toJson(myList, myFileOutputStreamWriter);
+
+}
