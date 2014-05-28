d9701fc12892ed00ce8a6f20030530eb7bafa4e5
sudov <vinay.farias@gmail.com>
2014-05-28 19:38:41 -0400

    ...

--- src/dbgen.java
+++ src/dbgen.java
@@ -4,8 +4,6 @@ import Gson;
 public static void main(String[] args){
 	
 	BufferedReader reader = new BufferedReader(new FileReader("cities1000.txt"));
-	HashMap<String, String> myList;
-	BufferedWriter writer = new BufferedWriter(new FileWriter(outputfilepath));
 
 	while(reader.read()){
 		System.out.println("wtf")
